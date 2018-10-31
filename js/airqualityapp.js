f4792b89c8587dfb56f5af8544d55b412217f04b
Jacob Johnson <john6805@stthomas.edu>
2018-10-30 22:49:54 -0500

    small refactor to incorporate Vue framework, added navbar to include about page

--- js/airqualityapp.js
+++ js/airqualityapp.js
@@ -1,56 +1,51 @@
 var app;
 var mymap;
 
-function Init()
-{
-    app = new Vue({
-        el: "#app",
-        data: {
-            location: "",
-            lat: "",
-            lng: "",
-            measurements: null
+Vue.component('aq-map', {
+    props: {},
+    template: '<div id="mapid" class="one-half column" style="margin-top: 10%; height:300px"></div>'
+})
+
+app = new Vue({
+    el: "#app",
+    data: {
+        location: "",
+        lat: "",
+        lng: "",
+        measurements: null
+    },
+    methods:{
+        updateFromMap: function () {
+            this.lat = mymap.getCenter().lat;
+            this.lng = mymap.getCenter().lng;
+            this.findNearestLocation();
         },
-    });
-    
-    // Map functions
-    
-    mymap = L.map('mapid').setView([51.505, -0.09], 13);
-    
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoid2ViZTAxMTkiLCJhIjoiY2pucGlndjJsMDY1bzN3bGthbHFkeW1yYyJ9.Bi-5WN5V7KN__SJGK-v9TQ', {
-                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                maxZoom: 18,
-                id: 'mapbox.streets',
-                accessToken: 'pk.eyJ1Ijoid2ViZTAxMTkiLCJhIjoiY2pucGlndjJsMDY1bzN3bGthbHFkeW1yYyJ9.Bi-5WN5V7KN__SJGK-v9TQ'
-                }).addTo(mymap);
-    
-    updateFromMap();
+        findNearestLocation: function () {
+            var request = {
+                url: "https://api.openaq.org/v1/locations?nearest=1&coordinates=" + app.lat + "," + app.lng,
+                dataType: "json",
+                success: function (data) { app.location = data.results[0].location; getAQData(data); }
+            };
+            $.ajax(request);
+        },
+        updateFromForm: function () {
+            mymap.panTo([this.lat,this.lng]);
+            this.findNearestLocation();
+        }
+    }
+});
 
-}
+// Map functions
 
-function updateFromMap()
-{
-    app.lat = mymap.getCenter().lat;
-    app.lng = mymap.getCenter().lng;
-    findNearestLocation();
-}
+mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-function updateFromForm() {
-    mymap.panTo([app.lat,app.lng]);
-    findNearestLocation();
-}
-
-
-function findNearestLocation()
-{
-    // Find nearest location
-    var request = {
-        url: "https://api.openaq.org/v1/locations?nearest=1&coordinates=" + app.lat + "," + app.lng,
-        dataType: "json",
-        success: function (data) { app.location = data.results[0].location; getAQData(data); }
-    };
-    $.ajax(request);
-}
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoid2ViZTAxMTkiLCJhIjoiY2pucGlndjJsMDY1bzN3bGthbHFkeW1yYyJ9.Bi-5WN5V7KN__SJGK-v9TQ', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox.streets',
+            accessToken: 'pk.eyJ1Ijoid2ViZTAxMTkiLCJhIjoiY2pucGlndjJsMDY1bzN3bGthbHFkeW1yYyJ9.Bi-5WN5V7KN__SJGK-v9TQ'
+            }).addTo(mymap);
+app.updateFromMap();
 
 function getAQData(data) {
     app.city = data.results.location;
@@ -63,8 +58,7 @@ function getAQData(data) {
 }
 
 function OpenAQData(data)
-{
-    
+{    
     app.measurements = data.results[0].measurements;
     console.log(app.measurements);
 }
