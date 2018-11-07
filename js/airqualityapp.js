83ac5bceabf58e6a1782b8eecc59ede6eb8372cc
Jacob Johnson <john6805@stthomas.edu>
2018-11-06 22:19:04 -0600

    finished table coloring and conversions, started particle filtering

--- js/airqualityapp.js
+++ js/airqualityapp.js
@@ -13,6 +13,7 @@ app = new Vue({
         cityRequest: "",
         dateFrom: "",
         dateTo: "",
+        filters: [],
         location: "",
         lat: "",
         lng: "",
@@ -76,27 +77,56 @@ app = new Vue({
                 marker.bindPopup(popupContent);
             });
         },
+        filterParticles: function (){
+            var hasParameter = false;
+            if(this.filters.length == 0)
+            {
+                this.addMarker();
+                return;
+            }
+            for(var j = 0; j < this.markerData.length; j++)
+            {
+                hasParameter = false
+                for(var i = 0; i < this.filters.length; i++)
+                {
+                    if(markerData[j][filters[i]] !== null)
+                    {
+                        hasParameter = true;
+                    }
+                }
+                if(!hasParameter)
+                {
+                    markerData.splice(j,1);
+                }
+            }
+            this.addMarker();
+        },
         indexColor: function (value, unit, parameter) {
             var color;
-            //var index = findAQIndex(value, unit, parameter);
-            if (value == null) {
+            var index = this.findAQIndex(value, unit, parameter);
+            if (index == null) {
                 color = "white";
-            } else if (value <= 50) {
+            } else if (index == 0) {
                 color = "rgb(0,228,0)";
-            } else if (value <= 100) {
+            } else if (index == 1) {
                 color = "rgb(255,255,0)";
-            } else if (value <= 150) {
+            } else if (index == 2) {
                 color = "rgb(255,126,0)";
-            } else if (value <= 200) {
+            } else if (index == 3) {
                 color = "rgb(255,0,0)";
-            } else if (value <= 300) {
+            } else if (index == 4) {
                 color = "rgb(143,63,151)";
-            } else if (value > 300) {
+            } else if (index == 5) {
                 color = "rgb(126,0,35)";
             }
             return {
                 'background-color': color
             }
+        },
+        minimumDate: function () {
+            var current = Date.now();
+            var minDate = current - 7776000000;
+            return Date(minDate).toISOString();
         },
         //Emailed Prof Marrinan about this. The values come back in different units.
         //If you look at page 4 in the pdf he linked on the assignment page,
@@ -108,52 +138,69 @@ app = new Vue({
             }
             if (parameter === "so2") {
                 if (unit == "µg/m³") {
-                    value = (value / 2.62);
+                    value = (value * 0.000000001);
                 }
                 if (unit == "ppm") {
-                    value = value * 1000;
+                    value = value / 1000;
                 }
-                if (value <= 35) {
-                    return 0;
+                if (value <= 35) { return 0; }
+                if (value <= 75) { return 1; }
+                if (value <= 185) { return 2; }
+                if (value <= 304) { return 3; }
+                if (value <= 604) { return 4; }
+                if (value > 604) { return 5; }
+            }
+            if (parameter === "no2") {
+                if (unit == "µg/m³") {
+                    value = (value * 0.000000001);
                 }
-                if (value <= 75) {
-                    return 1;
+                if (unit == "ppm") {
+                    value = value / 1000;
                 }
-                if (value <= 185) {
-                    return 2;
-                }
-                if (value <= 304) {
-                    return 3;
-                }
-                if (value <= 604) {
-                    return 4;
-                }
-                if (value > 604) {
-                    return 5;
-                }
+                if (value <= 53) { return 0; }
+                if (value <= 100) { return 1; }
+                if (value <= 360) { return 2; }
+                if (value <= 304) { return 3; }
+                if (value <= 649) { return 4; }
+                if (value > 649) { return 5; }
             }
             if (parameter === "co") {
                 if (unit == "µg/m³") {
-                    value = (value / 1.145) * 1000;
+                    value = (value * 0.000001);
                 }
-                if (value <= 35) {
-                    return 0;
+                if (value <= 4.4) { return 0; }
+                if (value <= 9.4) { return 1; }
+                if (value <= 12.4) { return 2; }
+                if (value <= 15.4) { return 3; }
+                if (value <= 30.4) { return 4; }
+                if (value > 30.4) { return 5; }
+            }
+            if (parameter === "o3") {
+                if (unit == "µg/m³") {
+                    value = (value * 0.000001);
                 }
-                if (value <= 75) {
-                    return 1;
-                }
-                if (value <= 185) {
-                    return 2;
-                }
-                if (value <= 304) {
-                    return 3;
-                }
-                if (value <= 604) {
-                    return 4;
-                }
-                if (value > 604) {
-                    return 5;
-                }
+                if (value <= .054) { return 0; }
+                if (value <= .070) { return 1; }
+                if (value <= .085) { return 2; }
+                if (value <= .105) { return 3; }
+                if (value <= .2) { return 4; }
+                if (value > .2) { return 5; }
+            }
+            if (parameter === "pm25") {
+                if (value <= 12) { return 0; }
+                if (value <= 35.4) { return 1; }
+                if (value <= 55.4) { return 2; }
+                if (value <= 150.4) { return 3; }
+                if (value <= 250.4) { return 4; }
+                if (value > 250.4) { return 5; }
+            }
+            if (parameter === "pm10") {
+                if (value <= 54) { return 0; }
+                if (value <= 154) { return 1; }
+                if (value <= 254) { return 2; }
+                if (value <= 354) { return 3; }
+                if (value <= 424) { return 4; }
+                if (value > 424) { return 5; }
             }
         }
     }
@@ -186,7 +233,8 @@ function getAQData() {
     var radius = Math.min(width, height) / 2;
 
     if (app.mode == "Latest") {
-        $.getJSON("https://api.openaq.org/v1/latest?coordinates=" + app.lat + "," + app.lng + "&radius=" + radius,
+        $.getJSON("https://api.openaq.org/v1/latest?coordinates=" 
+        + app.lat + "," + app.lng + "&radius=" + radius,
             function (response) {
                 app.markerData = [];
                 response.results.forEach(function (location) {
@@ -198,7 +246,8 @@ function getAQData() {
                         co: null,
                         so2: null,
                         no2: null,
-                        o3: null
+                        o3: null,
+                        date: null
                     }
                     location.measurements.forEach(function (item) {
                         if (item.parameter === "pm25") {
@@ -242,7 +291,8 @@ function getAQData() {
                         co: null,
                         so2: null,
                         no2: null,
-                        o3: null
+                        o3: null,
+                        date: item.date.local
                     }
                     if (item.parameter === "pm25") {
                         entry.pm25 = item;
